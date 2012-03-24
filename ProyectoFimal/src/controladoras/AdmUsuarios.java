e234837527f5727f9285f0b1dbde1b49037c0942
Luis Caiguaraico <luiscm0609@gmail.com>
2012-03-24 13:18:21 -0500

    comentarios

--- ProyectoFimal/src/controladoras/AdmUsuarios.java
+++ ProyectoFimal/src/controladoras/AdmUsuarios.java
@@ -16,11 +16,13 @@ public class AdmUsuarios {
         arUsuario = new ArrayList<Usuario>();
     }
     
+    // Ingresa un nuevo usuario
     public void registrarUsuario(String usuario, String contrasenha) {
         objUsuario = new Usuario(usuario, contrasenha);
         arUsuario.add(objUsuario);
     }    
     
+    // Validaque el usuario ingresado no sea vacío
     public boolean validaUsuario(String usuario) {
         if (usuario.equals("")) {
             return false;
@@ -29,6 +31,7 @@ public class AdmUsuarios {
         }                
     }
     
+    // Valida que la contraseña ingresa no sea vacía
     public boolean validaContrasenha(String contrasenha) {
         if (contrasenha.equals("")) {
             return false;
@@ -37,10 +40,13 @@ public class AdmUsuarios {
         }                
     }
     
+    // Devuelve la cantidad de usuarios ingresados
     public int getUsuarios() {
         return arUsuario.size();
     }
     
+    
+    // Busca si el usuario ingresado existe
     public boolean buscaUsuarioContrasenha(String usuario, String contrasenha) {
         for (Usuario user: arUsuario) {
             if (user.getUsuario().equals(usuario) && user.getContrasenha().equals(contrasenha)) {
