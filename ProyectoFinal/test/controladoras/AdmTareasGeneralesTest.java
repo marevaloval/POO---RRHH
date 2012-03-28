d97cd319a0b215578e1445118ceebc6eed4c336d
unknown <lcaiguaraicom@HN060331.gp.inet>
2012-03-28 09:32:31 -0500

    Recuperacion de proyecto

--- ProyectoFinal/test/controladoras/AdmTareasGeneralesTest.java
+++ ProyectoFinal/test/controladoras/AdmTareasGeneralesTest.java
@@ -1,8 +1,11 @@
-package proyectofimal;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladoras;
 
-import controladoras.AdmTareasGenerales;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * @author Grupo
--- ProyectoFinal/test/controladoras/AdmUsuariosTest.java
+++ ProyectoFinal/test/controladoras/AdmUsuariosTest.java
@@ -2,11 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyectofimal;
+package controladoras;
 
-import controladoras.AdmUsuarios;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
         
 public class AdmUsuariosTest {
     
